e454442dd17b903a3d168515b64de18c9ccaf98b
Nicolas Poupon <nicolas.poupon@epitech.eu>
2023-01-13 11:58:43 +0100

    [FIX] delete background comment

--- website/garrasi/src/App.vue
+++ website/garrasi/src/App.vue
@@ -1,7 +1,7 @@
-<template style="background-color: #00008B; display: flex; align-items: center; justify-content: center;">
+<template style="display: flex; align-items: center; justify-content: center;">
   <img alt="Garrasi logo" src="./assets/Garrasi.png">
   <HelloWorld msg="Welcome to Garrasi"/>
-  <div style="background-color: #00008B;align -items: center; justify-content: center; color: white; font-size: 2em; margin: 200px 200px;">
+  <div style="background-color: #00000;align -items: center; justify-content: center; color: black; font-size: 2em; margin: 200px 200px;">
     <div>
       <img src="./assets/avis1.png" alt="profile image">
       <p>"J'ai utilisé Garrasi pour trouver des avis sur un site de e-commerce avant d'acheter et j'ai été agréablement surpris par la qualité des avis. Cela m'a permis de faire un choix éclairé." - Martine</p>

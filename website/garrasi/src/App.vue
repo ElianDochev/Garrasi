f98b025d1e3f6285a49127dfdccd4d67ebc54d6d
Nicolas Poupon <nicolas.poupon@epitech.eu>
2023-01-13 11:56:59 +0100

    [ADD] website

---
+++ website/garrasi/src/App.vue
@@ -0,0 +1,52 @@
+<template style="background-color: #00008B; display: flex; align-items: center; justify-content: center;">
+  <img alt="Garrasi logo" src="./assets/Garrasi.png">
+  <HelloWorld msg="Welcome to Garrasi"/>
+  <div style="background-color: #00008B;align -items: center; justify-content: center; color: white; font-size: 2em; margin: 200px 200px;">
+    <div>
+      <img src="./assets/avis1.png" alt="profile image">
+      <p>"J'ai utilisé Garrasi pour trouver des avis sur un site de e-commerce avant d'acheter et j'ai été agréablement surpris par la qualité des avis. Cela m'a permis de faire un choix éclairé." - Martine</p>
+      <div class="stars">
+        <img v-for="n in 5" :key="n" src="./assets/star.png" alt="star">
+      </div>
+    </div>
+    <div>
+      <img src="./assets/avis2.png" alt="profile image">
+      <p>"J'ai utilisé Garrasi pour connaître l'avis de mes clients et c'est super" - Paul</p>
+      <div class="stars">
+        <img v-for="n in 4" :key="n" src="./assets/star.png" alt="star">
+      </div>
+    </div>
+    <div>
+      <img src="./assets/avis1.png" alt="profile image">
+      <p>"J'ai utilisé Garrasi vérifier si un site web était fiable et heureusement cela ma evité de me faire arnaquer !" - Séverine</p>
+      <div class="stars">
+        <img v-for="n in 5" :key="n" src="./assets/star.png" alt="star">
+      </div>
+    </div>
+  </div>
+
+</template>
+
+
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ website/garrasi/src/components/HelloWorld.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="hello" style="background-color: #00008B; display: flex; align-items: center; justify-content: center;">
+    <p style="color: white; font-size: 2em; margin: 200px 200px;">{{msg}}</p>
+  </div>
+</template>
+<script>
+export default {
+  name: 'HelloWorld',
+  data () {
+    return {
+      msg: 'Garrasi est un réseau social innovant qui vous permet de découvrir les commentaires et les avis des autres membres sur tous les sites web. Avec Garrasi, vous pouvez facilement obtenir des informations de confiance sur les sites web que vous visitez, sans avoir à parcourir des dizaines de sites différents.Notre plateforme est conçue pour vous aider à prendre des décisions informées lorsque vous naviguez sur internet. Nous savons à quel point il peut être difficile de trouver des avis fiables sur les produits et les services en ligne, c\'est pourquoi nous avons créé Garrasi.Notre réseau social est basé sur la communauté, et nous encourageons les membres à partager leurs expériences et leurs avis sur les sites web qu\'ils visitent. En utilisant Garrasi, vous pouvez facilement trouver des avis de personnes qui ont eu une expérience similaire à la vôtre, et vous pouvez également partager vos propres avis pour aider les autres.Garrasi est également une plateforme sécurisée et respectueuse de la vie privée. Nous protégeons vos données et nous nous engageons à ne jamais partager vos informations personnelles avec des tiers. Rejoignez dès maintenant notre communauté en ligne et profitez de tous les avantages de Garrasi. Nous sommes convaincus que vous apprécierez l\'expérience utilisateur unique que nous offrons, et nous espérons que vous nous aiderez à construire un meilleur internet pour tous.'
+    }
+  }
+}
+</script>
+
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
